d0014e53798ff7ec5e39f5de4f3402dcf9c6f02e
Omar Meriwani <omar.sirwan@gmail.com>
2017-05-24 15:43:34 +0300

    ---

---
+++ MVC Fundamentals/MVC Fundamentals/Models/Status2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC_Fundamentals.Models
+{
+    public class Status2
+    {
+        [Display(Name = "ID")]
+        public int ST_ID { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Please enter service Status Name")]
+        public string ST_NAME { get; set; }
+        [Display(Name = "Auother")]
+        [Required(ErrorMessage = "Please enter service Status Author")]
+        public string ST_AUTHOR { get; set; }
+        [Display(Name = "Data Entery Date")]
+        [Required(ErrorMessage = "Please enter service Status Entery Date")]
+        public DateTime ST_ENTRYDATE { get; set; }
+        [Display(Name = "Enable For Providers")]
+        public int PROVIDERENABLED { get; set; }
+        [Display(Name = "Enable For Requeset")]
+        public int FORREQUESTS { get; set; }
+        [Display(Name = "Send Email Notification")]
+        public int EMAIL_ENABLED { get; set; }
+        [Display(Name = "Reject Status")]
+        public int ST_REJECT { get; set; }
+
+    }
+}
---
+++ MVC Fundamentals/MVC Fundamentals/Repository/StatusRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.OracleClient;
+
+namespace MVC_Fundamentals.Repository
+{
+    public class StatusRepository
+    {
+        OracleConnection con = new OracleConnection("DATA SOURCE=OLTP2;PASSWORD=Shortcode;USER ID=Shortcode");
+        public List<Models.Status2> statusList()
+        {
+            List<Models.Status2> stl = new List<Models.Status2>();
+            OracleCommand oCommand = new OracleCommand("select * from tbl_status_sc",con);
+            con.Open();
+            OracleDataReader reader = oCommand.ExecuteReader();
+            while (reader.Read()) {
+                Models.Status2 st = new Models.Status2();
+                st.ST_ID = Convert.ToInt32( reader["ST_ID"]);
+                st.ST_NAME = reader["ST_NAME"].String;
+            }
+
+
+
+            return stl;
+        }
+    
+    }
+
+    
+}
