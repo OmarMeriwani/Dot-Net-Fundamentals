3af323332a264c5d30b5d73bfd3b27f8e99ae379
Omar Meriwani <omar.sirwan@gmail.com>
2017-06-05 15:41:12 +0300

    --

--- Thread class/Thread class/Program.cs
+++ Thread class/Thread class/Program.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Thread_class
 {
     class Program
     {
+        public static void MethodOfThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread i = {0}", i);
+                Thread.Sleep(1);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Thread t = new Thread(new ThreadStart(MethodOfThread));
+            t.Start();
+            for (int i = 0 ; i < 4; i++)
+            {
+                Console.WriteLine("Main thread");
+                Thread.Sleep(2);
+            }
+            t.Join();
+            Console.ReadLine();
         }
+     
+
+
     }
 }
