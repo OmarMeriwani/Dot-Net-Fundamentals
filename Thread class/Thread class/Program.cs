c10979f5fbfb32575f3c5d7c0f4c4f0d30679e70
Omar Meriwani <omar.sirwan@gmail.com>
2017-06-07 12:03:09 +0300

    --

--- Thread class/Thread class/Program.cs
+++ Thread class/Thread class/Program.cs
@@ -9,10 +9,39 @@ namespace Thread_class
 
     class Program
     {
-       
+        public static void MethodA()
+        {
+            Console.WriteLine("Thread A is running");
+            Thread.Sleep(1000);
+        }
+        public static void MethodB()
+        {
+            for (int i = 0; i < 100; i ++  )
+            {
+                if (i == 90)
+                {
+                    stopA = true;
+                    Console.WriteLine("B has gave an order to stop A");
+                }
+                Thread.Sleep(100);
+
+            }
+        }
+        static bool stopA = false ;
         static void Main(string[] args)
         {
-            
+            Thread a  = new Thread (new ThreadStart(() => 
+                {
+                    while (stopA != true )
+                        MethodA();
+                }));
+            a.Start();
+            Console.WriteLine("A has started");
+            Thread b = new Thread(new ThreadStart(MethodB));
+            b.Start();
+            Console.WriteLine("B has started");
+            Console.ReadLine();
+
         }
      
 
